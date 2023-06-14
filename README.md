6cdc21598c8418527d9f60e2f84655c604b68ff7
Adithya <adithya.pathipaka@hrs.de>
2023-06-14 10:43:44 +0200

    Updated the code to pull the VPC IP's

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# terraform-aws-vpc-ips
+# AWS Terraform Module to Pull the IPs from the Network Interfaces
+Terraform module to fetch the Ips from AWS VPC Network interfaces
--- data.tf
+++ data.tf
@@ -1,11 +1,40 @@
+locals {
+  is_vpc_passed                         = var.aws_vpc_id == null ? false : true
+  is_security_id_passed                 = length(var.security_group_id) > 0 ? true : false
+  is_aws_interface_endpoint_type_passed = length(var.aws_interface_endpoint_type) > 0 ? true : false
+ 
+}
 data "aws_network_interfaces" "network_interfaces" {
-  
-  filter {
-    name   = "vpc-id"
-    values = [var.var.aws_vpc_id]
+
+
+  dynamic "filter" {
+    for_each = local.is_vpc_passed ? [1] : []
+    content {
+      name   = "vpc-id"
+      values = [var.aws_vpc_id]
+    }
   }
-#    filter {
-#     name   = "interface-type"
-#     values = ["vpc_endpoint"]
-#   }
+
+  dynamic "filter" {
+    for_each = local.is_security_id_passed ? [1] : []
+    content {
+      name   = "group-id"
+      values = var.security_group_id
+    }
+  }
+
+  dynamic "filter" {
+    for_each = local.is_aws_interface_endpoint_type_passed ? [1] : []
+    content {
+      name   = "interface-type"
+      values = var.aws_interface_endpoint_type
+    }
+  }
+
+}
+
+
+data "aws_network_interface" "interface_info"{
+  count = length(data.aws_network_interfaces.network_interfaces.ids)
+  id = data.aws_network_interfaces.network_interfaces.ids[count.index]
 }
